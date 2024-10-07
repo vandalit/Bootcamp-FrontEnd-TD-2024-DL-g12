196626309e45c6272daf8b614849d37e21e547b3
Van <84937597+vandalit@users.noreply.github.com>
2024-10-07 13:40:12 -0300

    estructura html + modal + alert

--- Modulo_03/24_viajes-chile-p2-prueba/assets/js/index.js
+++ Modulo_03/24_viajes-chile-p2-prueba/assets/js/index.js
@@ -1,26 +1,26 @@
 import "../../node_modules/jquery/dist/jquery.js";
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
-// fondo para el navbar
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('navbar-bg');
-    } else {
-        navbar.classList.remove('navbar-bg');
-    }
-});
+// // fondo para el navbar
+// window.addEventListener('scroll', function() {
+//     const navbar = document.querySelector('.navbar');
+//     if (window.scrollY > 50) {
+//         navbar.classList.add('navbar-bg');
+//     } else {
+//         navbar.classList.remove('navbar-bg');
+//     }
+// });
 
-  // Smooth scrolling para los links del navbar //
-  $('.navbar-nav .nav-link').on('click', function(event) { //detecta clicks en el nav
-    var target = $(this).attr('href'); // obtiene el href del link
-    if ($(target).length) { // si el target existe
-      event.preventDefault(); // evita el comportamiento por defecto
-      $('html, body').animate({ // anima el scroll hacia el target
-        scrollTop: $(target).offset().top -90 //define hacia donde va el scroll
-      }, 100); // velocidad en ms de la animación
-    }
-  });
+//   // Smooth scrolling para los links del navbar //
+//   $('.navbar-nav .nav-link').on('click', function(event) { //detecta clicks en el nav
+//     var target = $(this).attr('href'); // obtiene el href del link
+//     if ($(target).length) { // si el target existe
+//       event.preventDefault(); // evita el comportamiento por defecto
+//       $('html, body').animate({ // anima el scroll hacia el target
+//         scrollTop: $(target).offset().top -90 //define hacia donde va el scroll
+//       }, 100); // velocidad en ms de la animación
+//     }
+//   });
 
 
 // Tooltip
@@ -51,3 +51,4 @@ function alert1() {
   $("#enviarBtn").click(function() {
     alert1();
   });
+
