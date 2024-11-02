1649cbe082fcc8a6ad8aa5fe2133ee3357a0584f
Van <84937597+vandalit@users.noreply.github.com>
2024-11-02 20:50:12 -0300

    workin

--- Modulo_07/99_4_firebase-1/firebase-I/src/App.vue
+++ Modulo_07/99_4_firebase-1/firebase-I/src/App.vue
@@ -1,8 +1,15 @@
-<script setup>
-</script>
-
 <template>
-</template>
-
-<style scoped>
-</style>
+    <BaseDatos />
+    <UsuariosRegistrados />
+  </template>
+  
+  <script setup>
+  import BaseDatos from './components/BaseDatos.vue'
+  import UsuariosRegistrados from './components/UsuariosRegistrados.vue'
+  </script>
+  
+  <style>
+  #app {
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+  </style>
